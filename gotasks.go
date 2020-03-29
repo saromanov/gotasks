68487c131d19e96b68407b25d99bf3849a021008
Sergey <xxsmotur@gmail.com>
2020-03-29 23:57:28 +0500

    FIxing of setting new task

--- gotasks.go
+++ gotasks.go
@@ -7,19 +7,22 @@ import (
 
 // GoTasks provides implementation of tasks
 type GoTasks struct {
-	mu    *sync.Mutex
+	mu    *sync.RWMutex
 	tasks map[string]func(context.Context) error
 }
 
 // New provides creating of the tasks instance
 func New() *GoTasks {
 	return &GoTasks{
-		mu:    &sync.Mutex{},
+		mu:    &sync.RWMutex{},
 		tasks: make(map[string]func(context.Context) error),
 	}
 }
 
 // Add provides addding of the task
 func (g *GoTasks) Add(name string, f func(context.Context) error) (string, error) {
-	return nil
+	g.mu.RLock()
+	defer g.mu.RUnlock()
+	g.tasks[name] = f
+	return "", nil
 }
