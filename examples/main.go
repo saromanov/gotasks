27c7ac6c8de9cc0c923e6ec9da3f2079a7590fa6
Sergey <xxsmotur@gmail.com>
2020-03-29 23:56:16 +0500

    initialization of the project with New method

--- examples/main.go
+++ examples/main.go
@@ -6,12 +6,21 @@ import (
 	"github.com/saromanov/gotasks"
 )
 
-func add(ctx context.Context) int {
+func add(ctx context.Context) error {
 	a := ctx.Value("a").(int)
 	b := ctx.Value("b").(int)
-	return a + b
+	context.WithValue(ctx, "add_func", a+b)
+	return nil
 }
+
+func sub(ctx context.Context) error {
+	a := ctx.Value("a").(int)
+	b := ctx.Value("b").(int)
+	context.WithValue(ctx, "sub_func", a-b)
+	return nil
+}
+
 func main() {
 	tasks := gotasks.GoTasks{}
-	tasks.Add(add)
+	tasks.Add("add", add)
 }
--- gotasks.go
+++ gotasks.go
@@ -1,12 +1,25 @@
 package gotasks
 
-import "context"
+import (
+	"context"
+	"sync"
+)
 
 // GoTasks provides implementation of tasks
 type GoTasks struct {
+	mu    *sync.Mutex
+	tasks map[string]func(context.Context) error
+}
+
+// New provides creating of the tasks instance
+func New() *GoTasks {
+	return &GoTasks{
+		mu:    &sync.Mutex{},
+		tasks: make(map[string]func(context.Context) error),
+	}
 }
 
 // Add provides addding of the task
-func (g *GoTasks) Add(f func(context.Context) error) error {
+func (g *GoTasks) Add(name string, f func(context.Context) error) (string, error) {
 	return nil
 }
