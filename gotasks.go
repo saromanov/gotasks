10266fee7c5c9a99831f09584cc1ce07d1c4c42b
Sergey <xxsmotur@gmail.com>
2020-03-30 00:53:17 +0500

    Adding options before execution of tasks

--- gotasks.go
+++ gotasks.go
@@ -4,9 +4,24 @@ import (
 	"context"
 	"errors"
 	"sync"
+	"time"
 )
 
 var errTaskNotFound = errors.New("task is not found")
+
+// ExecOption defines options for execution of the task
+type ExecOption func(*Option)
+
+type Option struct {
+	timeout time.Duration
+}
+
+// WithTimeout defines option with specific timeput
+func WithTimeout(d time.Duration) ExecOption {
+	return func(opt *Option) {
+		opt.timeout = d
+	}
+}
 
 // GoTasks provides implementation of tasks
 type GoTasks struct {
@@ -31,11 +46,21 @@ func (g *GoTasks) Add(name string, f func(context.Context) error) (string, error
 }
 
 // Exec provides execution of task
-func (g *GoTasks) Exec(name string) error {
+func (g *GoTasks) Exec(name string, opt ...ExecOption) error {
 	task, ok := g.tasks[name]
 	if !ok {
 		return errTaskNotFound
 	}
+
+	options := &Option{}
+	for _, o := range opt {
+		o(options)
+	}
 	go task.Method(context.Background())
 	return nil
+}
+
+func singleExec() {
+	ctx := context.Background()
+	ctx, _ = context.WithTimeout(ctx, 100*time.Millisecond)
 }
