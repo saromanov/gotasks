c6b94723c3f5c47ce3a00960e5527044d934a6f1
Sergey <xxsmotur@gmail.com>
2020-04-01 17:44:12 +0500

    Draft for worker implementation

--- worker.go
+++ worker.go
@@ -5,11 +5,44 @@ package gotasks
 type worker struct {
 	closeChan chan struct{}
 	closedChan chan struct{}
+	task *Task
 }
 
-func newWorker() *worker {
+func newWorker(t *Task) *worker {
 	return &worker{
+		t:t,
 		closeChan:     make(chan struct{}),
 		closedChan:    make(chan struct{}),
+	}
+}
+
+func (w *worker) run(){
+	jobChan, retChan := make(chan interface{}), make(chan interface{})
+	defer func() {
+		close(retChan)
+		close(w.closedChan)
+	}()
+
+	for {
+		select {
+		case w.reqChan <- workRequest{
+			jobChan:       jobChan,
+			retChan:       retChan,
+			interruptFunc: w.interrupt,
+		}:
+			select {
+			case payload := <-jobChan:
+				result := w.worker.Process(payload)
+				select {
+				case retChan <- result:
+				case <-w.interruptChan:
+					w.interruptChan = make(chan struct{})
+				}
+			case _, _ = <-w.interruptChan:
+				w.interruptChan = make(chan struct{})
+			}
+		case <-w.closeChan:
+			return
+		}
 	}
 }
